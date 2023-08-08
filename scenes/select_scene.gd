5e5bd79d813a1923f9c28a6c00f536152b49db6a
Darkborderman <reastw1234@gmail.com>
2023-08-08 14:54:23 +0800

    refactor: Add directory file scans

--- scenes/select_scene.gd
+++ scenes/select_scene.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+var dir_path = "res://levels/"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	generate_data_packs(dir_path)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func generate_data_packs(path: String):	
+	var dir = DirAccess.open(path)
+	if dir != null:
+		for file_name in dir.get_files():
+			print(file_name)
+			# create_level_btn('%s/%s' % [dir.get_current_dir(), file_name], file_name)
+	else:
+		print("An error occurred when trying to access the path.")
 	pass
+	
