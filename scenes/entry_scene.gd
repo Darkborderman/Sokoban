d7b859583e39c6fbedf9adcd8db26bb07b26b138
Darkborderman <reastw1234@gmail.com>
2023-08-08 14:43:58 +0800

    refactor: Add scrollbar container

--- scenes/entry_scene.gd
+++ scenes/entry_scene.gd
@@ -12,4 +12,4 @@ func _on_quit_button_pressed():
 
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game/game_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/select_scene.tscn")
