8d0ec9278e85564d1e44546c0f999cd734923fd1
Darkborderman <reastw1234@gmail.com>
2023-08-09 15:04:43 +0800

    refactor: Add button generation

--- scenes/select/level_button.gd
+++ scenes/select/level_button.gd
@@ -1,11 +1,6 @@
+class_name LevelButton
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func generate(index_count: int):
+	$Button.text = str(index_count)
--- scenes/select/level_category.gd
+++ scenes/select/level_category.gd
@@ -1,9 +1,12 @@
 class_name LevelCategory
 extends Control
 
+const LevelButton = preload("res://scenes/select/level_button.tscn")
+
 
 func generate(label: String, index_count: int):
 	$Label.text = label
 	for i in index_count:
-		
-		print(i)
+		var level_button = LevelButton.instantiate()
+		level_button.generate(i)
+		$ScrollContainer/HBoxContainer.add_child(level_button)
