407a8925df20d92832b551fb31b06fc29091b92a
Darkborderman <reastw1234@gmail.com>
2023-08-09 16:12:20 +0800

    refactor: Remove main scene

--- scenes/main_scene.gd
+++
@@ -1,7 +0,0 @@
-class_name MainScene
-extends Node2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# Load entry screen
-	get_tree().change_scene_to_file("res://scenes/entry_scene.tscn")
