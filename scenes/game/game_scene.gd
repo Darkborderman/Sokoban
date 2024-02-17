1238afa7a35fa559a8101f16c9380386c55accb3
Darkborderman <reastw1234@gmail.com>
2024-02-18 00:03:57 +0800

    Done UI separation

--- scenes/game/game_scene.gd
+++ scenes/game/game_scene.gd
@@ -21,8 +21,8 @@ func generate_level(level_data: Variant) -> void:
 	var best_moves = Global.get_best_record()
 	if best_moves <= 0:
 		best_moves = ""
-	$MarginContainer/VBoxContainer/BestMovesContainer/BestMovesLabel.text = "Best Moves: " + str(best_moves)
-	$MarginContainer/VBoxContainer/LevelContainer/LevelLabel.text = "Level: " + str(Global.level_index + 1)
+	$MarginContainer/HBoxContainer/VBoxContainer/BestMovesContainer/BestMovesLabel.text = "Best Moves: " + str(best_moves)
+	$MarginContainer/HBoxContainer/VBoxContainer/LevelContainer/LevelLabel.text = "Level: " + str(Global.level_index + 1)
 
 	# Cleanup stale level data
 	cleanup_children($Walls)
@@ -66,7 +66,7 @@ func generate_level(level_data: Variant) -> void:
 
 func complete() -> void:
 	Global.save_best_record(Global.current_level_moves)
-	$ProceedButton.show()
+	$MarginContainer2/HBoxContainer/VBoxContainer2/MarginContainer3.show()
 	$AudioStreamPlayer2D.stream = complete_sound
 	$AudioStreamPlayer2D.play()
 	level_completed = true
@@ -81,7 +81,7 @@ func _ready():
 
 
 func _process(_delta):
-	$MarginContainer/VBoxContainer/MovesContainer/MovesLabel.text = "Moves: " + str(Global.current_level_moves)
+	$MarginContainer/HBoxContainer/VBoxContainer/MovesContainer/MovesLabel.text = "Moves: " + str(Global.current_level_moves)
 	var goals = $Goals.get_child_count()
 	for i in $Goals.get_children():
 		if i.occupied:
