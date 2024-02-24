9e85ad90ca51f59ab8adcff403093bfeee606963
Darkborderman <reastw1234@gmail.com>
2024-02-24 15:46:29 +0800

    Upgrade to Godot 4.2

--- scenes/game/game_scene.gd
+++ scenes/game/game_scene.gd
@@ -75,7 +75,7 @@ func complete() -> void:
 func _ready():
 	if len(Global.level_data[Global.mod_pack_id][Global.level_pack_id]) <= Global.level_index:
 		# All level solved, return to main scene
-		get_tree().change_scene_to_file("res://scenes/main/main_scene.tscn")
+		get_tree().change_scene_to_file.bind("res://scenes/main/main_scene.tscn").call_deferred()
 	else:
 		generate_level(Global.level_data[Global.mod_pack_id][Global.level_pack_id][Global.level_index])
 
