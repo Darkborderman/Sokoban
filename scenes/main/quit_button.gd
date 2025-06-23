5796406280f57c6d6ee69a7af7408142db96433b
Darkborderman <reastw1234@gmail.com>
2025-06-24 00:54:50 +0800

    Update

--- scenes/main/quit_button.gd
+++ scenes/main/quit_button.gd
@@ -1,9 +1,10 @@
 extends MarginContainer
 
 
-func _on_button_pressed():
-	await get_tree().create_timer(0.1).timeout
-	get_tree().quit()
+func _ready():
+	if OS.has_feature("web"):
+		self.visible = false;
 
-	if OS.has_feature("web") and JavaScriptBridge.has_method("eval"):
-		JavaScriptBridge.eval("window.close();")
+
+func _on_button_pressed():
+	get_tree().quit()
