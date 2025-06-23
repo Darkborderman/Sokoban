d24c80d2ca4f47e837cfa4ec305bedafba916f3b
Darkborderman <reastw1234@gmail.com>
2025-06-24 00:45:33 +0800

    Update

--- scenes/main/quit_button.gd
+++ scenes/main/quit_button.gd
@@ -2,7 +2,8 @@ extends MarginContainer
 
 
 func _on_button_pressed():
+	await get_tree().create_timer(0.1).timeout
 	get_tree().quit()
-	if OS.has_feature("web"):
-		# OPTIONAL: ask JS to close the tab (works only if the tab was script-opened)
-		JavaScriptBridge.eval("window.close();")  # requires the JavaScriptBridge singleton
+
+	if OS.has_feature("web") and JavaScriptBridge.has_method("eval"):
+		JavaScriptBridge.eval("window.close();")
