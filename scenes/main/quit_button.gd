57c372a318e317646103aaf13f8278cbbd2b7a79
Darkborderman <reastw1234@gmail.com>
2025-06-24 00:18:01 +0800

    Add platform check on quit button

--- scenes/main/quit_button.gd
+++ scenes/main/quit_button.gd
@@ -3,3 +3,6 @@ extends MarginContainer
 
 func _on_button_pressed():
 	get_tree().quit()
+	if OS.has_feature("web"):
+		# OPTIONAL: ask JS to close the tab (works only if the tab was script-opened)
+		JavaScriptBridge.eval("window.close();")  # requires the JavaScriptBridge singleton
